35a6b48cdfb6d391092f1995442e167443cfced3
jeankory500 <124745650+jeankory500@users.noreply.github.com>
2023-08-29 20:10:17 +0000

    lastSave

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  // Write your code here
+  document.querySelector("#the-excuse").innerHTML = generateAllDomains();
+};
+let generateAllDomains = () => {
+  let who = ["you", "the", "our", "those"];
+  let action = ["big", "little", "wild", "lucky"];
+  let what = ["beach", "music", "city"];
+  let when = [".com", ".net", ".us"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  return (
+    who[whoIndex] + action[actionIndex] + what[whatIndex] + when[whenIndex]
+  );
 };
